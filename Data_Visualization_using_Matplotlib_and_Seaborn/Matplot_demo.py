8534d707bc6fda8935388ce52aaa94d8945c89f2
Ayush kumar <ayushkumar447332@gmail.com>
2025-12-08 03:25:53 +0530

    Data Visualization using Matplotlib and Seaborn

---
+++ Data_Visualization_using_Matplotlib_and_Seaborn/Matplot_demo.py
@@ -0,0 +1,30 @@
+# import matplotlib.pyplot as plt 
+# plt.plot([1, 2, 3, 4], [10, 20, 25, 30])
+# print("Ploat created successfully.")
+# plt.show()
+# years = [1990, 1992, 1994, 1996, 1998, 2000, 2003, 2005, 2007, 2010]
+# runs =  [500, 700, 1100, 1500, 1800, 1200, 1700, 1300, 900, 1500]
+# kohli = [0, 0, 500, 800, 1100, 1300, 1500, 1800, 1900, 2100]
+# sehwag = [0, 300, 800, 1200, 1500, 1700, 1600, 1400, 1000, 0]
+# Rohit = [50, 390, 600, 1400, 1000, 1100, 1300, 700, 1000, 2000]
+
+# plt.plot(years, runs)
+# plt.tight_layout()
+# plt.show()
+# with plt.xkcd():
+#     plt.plot(years, kohli, label="Kohli")
+#     plt.plot(years, sehwag, label="Sehwag")
+#     plt.title("Epic Battle of the Batsmen")
+#     plt.legend()
+#     plt.show()
+    
+# import numpy as np
+
+# for i in range(50):
+#     plt.plot(np.random.rand(100), linewidth=1)
+
+# plt.title("Too Much Data Can Be Confusing!")
+# plt.grid(True)
+# plt.tight_layout()
+# plt.show()
+# print("Plot displayed successfully.")
